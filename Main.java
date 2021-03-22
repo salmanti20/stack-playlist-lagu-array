d52bb5798169a7a60de05d442ffdfb4c37792234
salman alfariji <79994933+salmanti20@users.noreply.github.com>
2021-03-22 15:04:30 +0700

    tugas sesi 4 algoritma dan struktur data
    tugas sesi 4 algodata membuat stack playlist array dengan java 
    salman alfariji 20200040033

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main( String[] arg)
+    {
+        Menu menu = new Menu();
+
+        menu.loadPlaylist();
+        menu.runMenu();
+
+    }
+
+}
